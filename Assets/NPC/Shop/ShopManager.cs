2c35791a59eb0aa7471874e9246d80ce330f5679
Simon <78388075+ActuallyRaspy@users.noreply.github.com>
2025-05-05 12:05:38 +0200

    Submarine UI
    Added submarine sprite.
    added ui for enter/exit shop.
    added buttons for buying 3 upgrades (not functional, only visual atm.)
    pause/unpause while in shop (bug: diver can still rotate while frozen.)

---
+++ Assets/NPC/Shop/ShopManager.cs
@@ -0,0 +1,107 @@
+using UnityEngine;
+
+public class ShopManager : MonoBehaviour
+{
+    public GameObject shopUI;
+    public GameObject openShopText;
+    public GameObject closeShopText;
+    public GameObject btnAirTube;
+    public GameObject btnFillAir;
+    public GameObject btnFins;
+    // public playerStats playerstats;
+    public float money;
+    public ShopTrigger shopTrigger;
+    public bool shopMenuIsOpen = false;
+
+    public void Start()
+    {
+        openShopText.SetActive(false);
+        closeShopText.SetActive(false);
+
+        btnAirTube.SetActive(false);
+        btnFillAir.SetActive(false);
+        btnFins.SetActive(false);
+    }
+
+    void Update()
+    {
+
+        //if in range and menu not open
+        if (shopTrigger.playerInRange && !shopMenuIsOpen)
+        {
+            openShopText.SetActive(true);
+            closeShopText.SetActive(false);
+        }
+
+        //if menu open
+        if (shopMenuIsOpen)
+        {
+            openShopText.SetActive(false);
+            closeShopText.SetActive(true);
+        }
+
+        //if not in range
+        if (!shopTrigger.playerInRange)
+        {
+            if (shopMenuIsOpen)
+            {
+                CloseShop();
+            }
+            closeShopText.SetActive(false);
+            openShopText.SetActive(false);
+        }
+
+
+        if (shopTrigger.playerInRange && Input.GetKeyDown(KeyCode.E) && !shopMenuIsOpen)
+        {
+            Debug.Log("E pressed 1");
+
+            OpenShop();
+        }
+
+        else if (shopMenuIsOpen && Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("E pressed 2");
+
+            CloseShop();
+        }
+    }
+
+    public void OpenShop()
+    {
+       Time.timeScale = 0f; // Pause the game if you want
+        
+        btnAirTube.SetActive(true);
+        btnFillAir.SetActive(true);
+        btnFins.SetActive(true);
+        shopMenuIsOpen = true;
+    }
+
+    public void CloseShop()
+    {
+        Time.timeScale = 1f;
+        btnAirTube.SetActive(false);
+        btnFillAir.SetActive(false);
+        btnFins.SetActive(false);
+        shopMenuIsOpen = false;
+    }
+
+    public void BuyUpgrade(int cost, string upgradeType)
+    {
+        if (money >= cost)
+        {
+            money -= cost;
+
+            switch (upgradeType)
+            {
+                //case "Speed":
+                //    playerStats.moveSpeed += 1f;
+                //    break;
+                //case "Oxygen":
+                //    playerStats.maxAir += 10f;
+                //    break;
+                //    // Add more cases as needed
+            }
+        }
+    }
+}
