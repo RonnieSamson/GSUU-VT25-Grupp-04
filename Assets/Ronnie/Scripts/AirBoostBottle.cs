3095e1f1178ad06c163e90019530ee6d6ff345f8
Christian Vittaniemi <crv87@outlook.com>
2025-05-08 16:14:38 +0200

    Airpressure

--- Assets/Ronnie/Scripts/AirBoostBottle.cs
+++ Assets/Ronnie/Scripts/AirBoostBottle.cs
@@ -4,6 +4,15 @@ public class AirBoostBottle : MonoBehaviour
 {
   public float airBoostAmount = 15f;
   public float boostDuration = 15f;
+
+  [SerializeField] private AudioClip airpressureSound;
+    private AudioSource audioSource;
+
+  private void Start()
+  {
+    audioSource = GetComponent<AudioSource>();
+  }
+  
   private void OnTriggerEnter2D(Collider2D other)
   {
     if (other.CompareTag("Player"))
@@ -16,6 +25,16 @@ public class AirBoostBottle : MonoBehaviour
         // Add air boost to the diver
         timer.AddAir(airBoostAmount);
         diver.ActivateBoost(boostDuration);
+        // Play sound effect
+        if (audioSource != null && airpressureSound != null)
+        {
+          audioSource.PlayOneShot(airpressureSound);
+        }
+        else
+        {
+          Debug.LogWarning("AudioSource or airpressureSound is not assigned in AirBoostBottle.");
+        }
+        
       }
       Destroy(gameObject);
     }
