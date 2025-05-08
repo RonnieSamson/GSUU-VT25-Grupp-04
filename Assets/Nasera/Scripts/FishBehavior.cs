55804b8bcd073d60a7e4c8f112e627ec1e40d082
NaseraBoujrad <boujradnasera@gmail.com>
2025-05-08 18:19:53 +0200

    Revert "Merge pull request #42 from RonnieSamson/feature/day-night-cycle"
    This reverts commit 1ff437d8b0571f5651ca0cffd2106d0dbd6ef3d9, reversing
    changes made to 7ee12b4ded399f113e4cee3e9edae119d5b7ac34.

--- Assets/Nasera/Scripts/FishBehavior.cs
+++ Assets/Nasera/Scripts/FishBehavior.cs
@@ -4,61 +4,40 @@ public class FishBehavior : MonoBehaviour
 {
     public float speed = 2f;
     public float distance = 5f;
-    public float fleeSpeed = 6f;
-    public float fleeDuration = 2f;
-    public float detectionRadius = 3f;
-    public LayerMask threatLayer;
 
     private Vector3 startPos;
     private bool movingRight = true;
     private SpriteRenderer[] spriteRenderers;
 
-    private bool isFleeing = false;
-    private float fleeTimer = 0f;
-
     void Start()
     {
         startPos = transform.position;
         spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
+
+        // Sätt rätt startvändning
         foreach (var sr in spriteRenderers)
             sr.flipX = !movingRight;
     }
 
     void Update()
     {
-        // Kolla om något hot är nära
-        Collider2D threat = Physics2D.OverlapCircle(transform.position, detectionRadius, threatLayer);
-        if (threat && !isFleeing)
+        // Rörelse
+        float moveStep = speed * Time.deltaTime;
+        if (movingRight)
+            transform.position += Vector3.right * moveStep;
+        else
+            transform.position += Vector3.left * moveStep;
+
+        // Vänd vid max distans
+        float movedDistance = Vector3.Distance(transform.position, startPos);
+        if (movedDistance >= distance)
         {
-            isFleeing = true;
-            fleeTimer = fleeDuration;
-            movingRight = transform.position.x < threat.transform.position.x; // fly motsatt riktning
+            movingRight = !movingRight; // Byt håll
+            startPos = transform.position; // Starta ny vända från nuvarande position
+
+            // Vänd alla fiskar visuellt
             foreach (var sr in spriteRenderers)
                 sr.flipX = !movingRight;
-        }
-
-        if (isFleeing)
-        {
-            fleeTimer -= Time.deltaTime;
-            if (fleeTimer <= 0f)
-                isFleeing = false;
-        }
-
-        float currentSpeed = isFleeing ? fleeSpeed : speed;
-        float moveStep = currentSpeed * Time.deltaTime;
-        transform.position += (movingRight ? Vector3.right : Vector3.left) * moveStep;
-
-        // Byt håll vid max distans (bara om inte flyr)
-        if (!isFleeing)
-        {
-            float movedDistance = Vector3.Distance(transform.position, startPos);
-            if (movedDistance >= distance)
-            {
-                movingRight = !movingRight;
-                startPos = transform.position;
-                foreach (var sr in spriteRenderers)
-                    sr.flipX = !movingRight;
-            }
         }
     }
 }
